123b91c042e69f4d988545ece9db4972e6f3b33a
Marian <marian@Marians-iMac.local>
2020-04-18 01:31:02 -0400

    Comments

--- linked_list.cpp
+++ linked_list.cpp
@@ -7,36 +7,71 @@ This is the implementation of the outline in linked_list.h
 
 #include "linked_list.h"
 
+// Linked list constructor
 linked_list::linked_list(){
+    // When a list starts it has no length
     length = 0;
+
+    // The first link is like a placeholder, has none before, none after, and no value
     first_link = {nullptr, 0, nullptr};
+
+    // At the start of list, the first link is the same last
     last_link = &first_link;
 }
 
+// Linked list push function
+// Takes in a value to push to the end of the list
 void linked_list::push(int value){
+    // Add a link to the end of the list
     last_link->next_link = new link();
+
+    // Initialize the new link with the pushed value
     *(last_link->next_link) = {last_link, value, nullptr};
+
+    // Push forward the last link
     last_link = last_link->next_link;
+
+    // Increase the length of the list
     length++;
 }
 
+// Linked list pop function
+// Returns the last value of the list and deletes it from the list
 int linked_list::pop(){
+    // If there are no items in the list, it cannot pop anything off of the list
     if(length == 0) return 0;
-    link* current_link = last_link->previous_link;
+
+    // Store the value of the last link in the list
     int temp = last_link->value;
+
+    // Store the second last link 
+    link* current_link = last_link->previous_link;
+
+    // Delete the last link
     delete current_link->next_link;
+
+    // Make the second last link the last link
     current_link->next_link = nullptr;
     last_link = current_link;
+
+    // Shrink the list size
     length--;
+
+    // Return the value of the pop'd value 
     return temp;
 }
 
+// Returns the value of the index specified
 int linked_list::operator[](int index){
+    // If the index is greater then the length or the index is smaller then 0, the index is out of bounds
     if (index + 1 > length || index < 0) return 0;
 
+    // Start the iteration at the first link
     link* current_link = &first_link;
-    for(int i = 0; i <= index; i++){
-        current_link = current_link->next_link;
-    }
+
+    // Iterate through the list through to the index
+    for(int i = 0; i <= index; i++) current_link = current_link->next_link;
+
+    // Return the index value
     return current_link->value;
 }
--- linked_list.h
+++ linked_list.h
@@ -8,23 +8,38 @@ This is the header file for my linked list library
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+// The linked list is built up of links (24 byte size, 1/3 the storage efficency of a normal array)
 struct link{
+    // Stores the previous link in the chain
     link* previous_link;
+
+    // Stores the actual value
     int value;
+
+    // Stores the next link in the chain
     link* next_link;
 };
 
 class linked_list{
     public:
+        // The list has a length
         int length;
+
+        // Starting link
         link first_link;
+
+        // End link
         link* last_link;
+
+        // List constructor
         linked_list();
         //~linked_list();
 
+        // Push & pop functions
         void push(int value);
         int pop();
 
+        // Index function
         int operator[](int index);
 };
 
