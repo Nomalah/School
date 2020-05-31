7fb3eb3786c139e9122c2b1a7b1cb781148d1e48
Marian <marian@Marians-iMac.local>
2020-05-31 17:22:05 -0400

    General Include - Needs work

---
+++ nmlh/nmlh.h
@@ -0,0 +1,12 @@
+/* This File will include all other files, 
+ * and if you are importing to your own project,
+ * this is most likely the way to go.
+ * 
+ * Copyright May 29 2020 Dallas Hart
+ */
+
+#pragma once
+
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl3.h>
