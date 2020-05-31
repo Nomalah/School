14b5269ad13188e14ac112723fe6a921b0bd399f
Marian <marian@Marians-iMac.local>
2020-05-31 17:22:29 -0400

    Shader functionality

---
+++ nmlh/gl/util/nmlh_shader.cpp
@@ -0,0 +1,120 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
+#include "../nmlh_log.hpp"
+#include "nmlh_shader.h"
+
+namespace nmlh::gl::util{
+    const unsigned shaderTypes = 3;
+
+    shader::shader(const std::string& shaderSource) : m_RendererId(glCreateProgram()){
+        std::ifstream shaderFile(shaderSource);
+
+        std::stringstream shaders[shaderTypes];
+
+        enum shaderType{
+            NONE = -1,
+            VERTEX = 0,
+            FRAGMENT = 1,
+            GEOMETRY = 2,
+        };
+        
+        shaderType curShader = shaderType::NONE;
+
+        std::string line = "";
+        while(getline(std::cin, line)){
+            if(line.find("#shader ") != std::string::npos){
+                if(line.find("vertex", 8) != std::string::npos)
+                    curShader = shaderType::VERTEX;
+                else if(line.find("fragment", 8) != std::string::npos)
+                    curShader = shaderType::FRAGMENT;
+                else if(line.find("geometry", 8) != std::string::npos)
+                    curShader = shaderType::GEOMETRY;
+            }else{
+                if(curShader != shaderType::NONE)
+                    shaders[curShader] << line;
+            }
+        }
+
+        GLuint shaderId[shaderTypes];
+        for(unsigned i = 0; i < shaderTypes; i++){
+            std::string shader = shaders[i].str();
+            switch(i){
+                case shaderType::VERTEX:
+                    shaderId[i] = compileShader(GL_VERTEX_SHADER, shader);
+                    break;
+                case shaderType::FRAGMENT:
+                    shaderId[i] = compileShader(GL_FRAGMENT_SHADER, shader);
+                    break;
+                case shaderType::GEOMETRY:
+                    shaderId[i] = compileShader(GL_GEOMETRY_SHADER, shader);
+            }
+            glAttachShader(m_RendererId, shaderId[i]);
+        }
+        glLinkProgram(m_RendererId);
+
+        // Check if the program is a valid shader
+        GLint success;
+        glValidateProgram(m_RendererId);
+        glGetProgramiv(m_RendererId, GL_VALIDATE_STATUS, &success);
+        if(success == GL_FALSE){
+            int messageLength;
+            glGetProgramiv(m_RendererId, GL_INFO_LOG_LENGTH, &messageLength);
+
+            char* errMessage = new char[messageLength];
+            if(errMessage == nullptr){
+                LOG_ERR("[Alloc Fail] ", "char* message = new char[messageLength];", 3);
+            }else{
+                glGetProgramInfoLog(m_RendererId, messageLength, &messageLength, errMessage);
+                LOG_ERR("[Shader Vallidation Failed] ", errMessage, 2);
+            }
+
+            m_RendererId = 0;
+        }
+
+        for(unsigned i = 0; i < shaderTypes; i++)
+            glDeleteShader(shaderId[i]);
+    }
+
+    GLuint shader::compileShader(GLenum type, const std::string& source){
+        GLuint shaderId = glCreateShader(type);
+        const char* shaderSrc = source.c_str();
+
+        glShaderSource(shaderId, 1, &shaderSrc, nullptr);
+        glCompileShader(shaderId);
+
+        // Check if the compile was successful
+        GLint success;
+        glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+        if(success == GL_FALSE){
+            int messageLength;
+            glGetShaderiv(m_RendererId, GL_INFO_LOG_LENGTH, &messageLength);
+
+            char* errMessage = new char[messageLength];
+            if(errMessage == nullptr){
+                LOG_ERR("[Alloc Fail] ", "char* message = new char[messageLength];", 3);
+            }else{
+                glGetShaderInfoLog(m_RendererId, messageLength, &messageLength, errMessage);
+                LOG_ERR("[Shader Vallidation Failed] ", errMessage, 2);
+            }
+
+            return 0;
+        }
+        
+        return shaderId;
+    }
+
+    GLint shader::getUniformLocation(const std::string& uniformName){
+        if(m_UniformLocation.find(uniformName) != m_UniformLocation.end())
+            return m_UniformLocation[uniformName];
+        
+        GLint uniformLocation = glGetUniformLocation(m_RendererId, uniformName.c_str());
+        if(uniformLocation == -1){
+            LOG_ERR("[Invalid Uniform] " + uniformName, "", 3);
+        }
+        m_UniformLocation[uniformName] = uniformLocation;
+        return uniformLocation;
+    }
+}
---
+++ nmlh/gl/util/nmlh_shader.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <OpenGL/gl3.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <unordered_map>
+
+namespace nmlh::gl::util{
+    class shader{
+        private:
+            GLuint m_RendererId;
+            std::unordered_map<std::string, GLuint> m_UniformLocation;
+            
+            GLint getUniformLocation(const std::string& uniformName);
+            GLuint compileShader(GLenum type, const std::string& source);
+        public:
+            // Constructor creates a shader from a GLSL file
+            shader(const std::string& shaderSource);
+            ~shader(){
+                glUseProgram(0);
+                glDeleteProgram(m_RendererId);
+            }
+
+            GLuint get_id(){return m_RendererId;};
+            void bind() const{glUseProgram(m_RendererId);};
+            void unbind() const{glUseProgram(0);};
+
+
+            void setUniform1i(const std::string& uniformName, glm::vec<1, int, glm::defaultp> uniformValues){
+                glUniform1iv(getUniformLocation(uniformName), 1, &uniformValues[0]);
+            };
+            
+            void setUniform4f(const std::string& uniformName, glm::vec4 uniformValues){
+                glUniform4fv(getUniformLocation(uniformName), 1, &uniformValues[0]);
+            };
+
+            void setUniformMatrix4fv(const std::string& uniformName, glm::mat4 uniformValues){
+                glUniformMatrix4fv(getUniformLocation(uniformName), 1, GL_FALSE, &uniformValues[0][0]);
+            };
+
+
+        private:
+            // Disallow Copy & move semantics with shaders
+            shader(const shader&);
+            shader(shader&&);
+            shader& operator=(const shader&);
+            shader& operator=(shader&&);
+    }; 
+}
