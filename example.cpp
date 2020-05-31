d7e874c66df17f13443d9d44e05ea2c41aa9d793
Nomalah <marian@Marians-iMac.local>
2020-05-31 19:41:07 -0400

    New Example

--- example.cpp
+++ example.cpp
@@ -1,6 +1,49 @@
+// EXAMPLE PROGRAM
+// Draws a Square to the screen
+
+// Irrelivent to actuall program
 #include <iostream>
 
+// In the final version, you should only have to include this file
+#include "nmlh/nmlh.h"
+
 int main(){
-    std::cout << "Hello World" << std::endl;
+    // You should not have to do this in the final version
+    GLFWwindow* window = nullptr;
+
+    // GLFW did not initialize correctly
+    if(!glfwInit()){
+        return -1;
+    }
+    
+    /* Create a windowed mode window and its OpenGL context */
+    // Set the OpenGL version to 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Make OpenGL forward compatable
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Use the OpenGL core Profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a 960px * 540px window
+    window = glfwCreateWindow(960, 540, "NMLH example", NULL, NULL);
+
+    // If the window was not created successfully
+    if (!window){
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    // Set Framerate to Vsync
+    glfwSwapInterval(1);
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+    glfwTerminate();
     return 0;
 }
