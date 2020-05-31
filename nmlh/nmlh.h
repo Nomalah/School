564886874ed15ce735e62c73e111f6072455c59c
Nomalah <marian@Marians-iMac.local>
2020-05-31 19:41:32 -0400

    Should be multi-platform

--- nmlh/nmlh.h
+++ nmlh/nmlh.h
@@ -1,4 +1,5 @@
-/* This File will include all other files, 
+/* 
+ * This File will include all other files, 
  * and if you are importing to your own project,
  * this is most likely the way to go.
  * 
@@ -7,6 +8,18 @@
 
 #pragma once
 
-#include <glm/glm.hpp>
+#if defined(__APPLE__)
+    #include <OpenGL/gl3.h>
+#else
+    #include <GL/gl3.h>
+#endif
+
+// Stop gl.h from being included
+#define __gl_h_
 #include <GLFW/glfw3.h>
-#include <OpenGL/gl3.h>
+
+#include <glm/glm.hpp>
+
+#include "gl/core/nmlh_vertex_buffer.h"
+#include "gl/util/nmlh_shader.h"
+#include "nmlh_log.hpp"
