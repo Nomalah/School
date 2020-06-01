6000ac6a2e7fa73ab6b1d2a21c348dfef385bae1
Nomalah <marian@Marians-iMac.local>
2020-06-01 12:23:20 -0400

    Formating for logging - fix

--- nmlh/nmlh_log.hpp
+++ nmlh/nmlh_log.hpp
@@ -8,10 +8,10 @@
     {                                                                   \
         std::string __clr;                                              \
         switch(err_lvl){                                                \
-            case 1: __clr = "\033[38;5;9m [Fatal]\n\t"; break;          \
-            case 2: __clr = "\033[38;5;11m [Error]\n\t"; break;         \
-            case 3: __clr = "\033[38;5;10m [Warning]\n\t"; break;       \
-            default: __clr = "\033[38;5;14m [Notif]\n\t"; break;        \
+            case 1: __clr = "\033[38;5;9m[Fatal]\n\t"; break;          \
+            case 2: __clr = "\033[38;5;11m[Error]\n\t"; break;         \
+            case 3: __clr = "\033[38;5;10m[Warning]\n\t"; break;       \
+            default: __clr = "\033[38;5;14m[Notif]\n\t"; break;        \
         }                                                               \
         std::cerr << __clr << message << data << "\033[38;5;15m\n";     \
     }
