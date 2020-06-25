a2dba53a955e31304eae075120ccb66496173816
Nomalah <marian@Marians-iMac.local>
2020-06-25 13:12:25 -0400

    Added Data Freeing

--- nmlh_dynamic_alloc.hpp
+++ nmlh_dynamic_alloc.hpp
@@ -1,6 +1,9 @@
+#pragma once
+
 #include <cstddef>
 #include <cstdint>
 #include <unistd.h>
+#include <sys/mman.h>
 
 namespace nmlh{
     const size_t KB = 1024;
@@ -22,6 +25,11 @@ namespace nmlh{
         inline bool used() const {return block_header & 1;}
 
         /**
+         * @brief Returns if the memory block is in a private page sector
+         */
+        inline bool private_sector() const {return block_header & 2;}
+
+        /**
          * @brief Returns the size of the memory block data
          */
         inline size_t size() const {return block_header & ~(sizeof(word_t) - 1);}
@@ -40,30 +48,34 @@ namespace nmlh{
         byte_t* brk = (byte_t*)this + sizeof(page_sector);
 
         void* sbrk(size_t inc);
+        memory_block* request(size_t size);
         inline memory_block* first_block() const {return (memory_block*)(this + 1);}
     };
 
     size_t align(size_t size){return (size + sizeof(word_t) - 1) & ~(sizeof(word_t) - 1);}
 
     class allocator{
-        private:
-            memory_block* request(page_sector* desired_sector, size_t size);
-
+        private:    
+            memory_block* find_free_block(size_t block_size){return nullptr;}
+            memory_block* get_memory_block(void* data_ptr){return (memory_block*)data_ptr - 1;}
         public:
             allocator();
             ~allocator();
 
             template<class C>
             C* alloc(size_t count);
+           
+            template<class C>
+            void free(C*& free_ptr);
 
             /*
-            the library has a specialization for the void-type
+            The library has a specialization for the void-type
 
             template<>
             void* alloc<void>(size_t size);
+            template<>
+            void free<void>(void*& free_ptr);
             */
-           
-            void free(void* free_ptr);
 
         private:
             page_sector* first_sector = nullptr;
@@ -72,3 +84,6 @@ namespace nmlh{
             size_t page_sector_default_size = page_size * 256;
     };
 }
+
+// Include the implementation of the template functions
+#include "nmlh_dynamic_alloc_imp.hpp"
