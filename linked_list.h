916b56dc35e3d82e20c0ece478cdaeb068847309
Nomalah <36043275+Nomalah@users.noreply.github.com>
2023-07-22 11:49:07 -0400

    Upload

--- linked_list.h
+++ linked_list.h
@@ -8,6 +8,10 @@ This is the header file for my linked list library
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+#include <memory> 
+#include <cstddef>
+#include <cstdlib>
+#include <list>
 #include <initializer_list>
 
 // The linked list is built up of links (24 byte size, 1/3 the storage efficency of a normal array)
@@ -49,6 +53,36 @@ class linked_list{
 
         // Index function
         int& operator[](int index);
+};
+
+template<class T, class Allocator = std::allocator<T>>
+class d_linked_list{
+    public:
+        typedef T value_type;
+        typedef Allocator allocator_type;
+        typedef T& reference;
+        typedef const T& const_reference;
+        typedef Allocator::pointer pointer;
+        typedef Allocator::const_pointer const_pointer;
+        typedef std::ptrdiff_t difference_type;
+        typedef std::size_t size_type;
+
+        void push_front(const T& val);
+        void push_front(T&& val);
+        void push_back(const T& val);
+        void push_back(T&& val);
+
+        void pop_front() noexcept;
+        void pop_back() noexcept;
+
+    private:
+        template<class T>
+        struct link{
+            link<T>* previous;
+            T data;
+            link<T>* next;
+        }
+
 };
 
 #endif // LINKED_LIST_H
